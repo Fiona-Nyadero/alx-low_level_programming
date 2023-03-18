89e3c9717bcd48f7b567d866b29a294231b1c714
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-18 02:24:09 +0000

    0. Positive anything negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,7 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there *
- /**
-  * main - main function
-  * No Description:
-  * Return: 0
-  */
+/* betty style doc for function main goes there */
 
 int main(void)
 {
