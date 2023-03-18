5116325dc3f2156ff6a1e4ca1b472945521e32ae
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-18 02:22:29 +0000

    0. Positive anything negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,12 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there *
-  /*
+ /**
   * main - main function
   * No Description:
   * Return: 0
   */
+
 int main(void)
 {
 	int n;
@@ -19,15 +20,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf( int n; "%d is positive\n");
+		printf( "%d is positive\n");
 	}
 	else if (n == 0)
 	{
-		printf( int n; "%d is zero\n");
+		printf( "%d is zero\n");
 	}
 	else
 	{
-		printf( int n; "%d is negative\n");
+		printf( "%d is negative\n");
 	}
 	return (0);
 }
