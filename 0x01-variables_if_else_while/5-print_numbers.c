7008e8aa14ef5e05693d5a3246f7ffb487f2b042
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-19 14:01:50 +0000

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* Printing all single digit numbers */
+
+/* betty style */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+	
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	return (0);
+}
