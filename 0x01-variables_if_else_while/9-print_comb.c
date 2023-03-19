3982d88cbfc1cd6c2d60b2e1546028bc7ba016bd
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-19 18:23:15 +0000

    9. All possible single-digit combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* Printing all single digit numbers with putchar */
+
+/* betty style */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
