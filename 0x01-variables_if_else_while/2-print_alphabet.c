3b57361fb8561e745db737f76af880dae7cd66d2
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-19 10:47:15 +0000

    2. The alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Printing alphabet in lowercase */
+
+/* betty style */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char x
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x\n);
+	}
+	return (0);
+}
