4b7cab23404a87033ad0677fc3d6ae58c60e2310
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-20 22:14:32 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
-int puts(const char *("Programming is like building a multilingual puzzle");
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
