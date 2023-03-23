563bb68655ceaf0fac3bfce689f6a1d1f3a835b0
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-23 02:28:08 +0000

    6. Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _abs - function to compute the absolute value
+ *
+ * @x: parameter to be printed
+ *
+ * Return: always x
+ */
+
+int _abs(int x)
+{
+
+	if (x < 0)
+		x = -(x);
+	else if (x >= 0)
+		x = x;
+	return (x);
+}
