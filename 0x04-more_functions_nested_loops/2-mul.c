182b2eb115e24a30018dbd2d211f53635578578a
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-24 01:30:38 +0000

    3. The numbers speak

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: parameter to multiply
+ * @b: parameter to multiply
+ *
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	putchar(a * b);
+	return (0);
+}
