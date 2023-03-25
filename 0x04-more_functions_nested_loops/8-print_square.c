12468e5e2342bb86e71c0683032aa9d53c56cf35
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-25 02:46:34 +0000

    8. Every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: number of times character # prints to form square
+ *
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int x;
+		int y;
+
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
