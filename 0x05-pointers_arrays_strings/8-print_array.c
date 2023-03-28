c219931691fed9c36688ab193b8329d9e858b1da
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-28 22:24:17 +0000

    9. Copy the string

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,26 +1,22 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_array -  prints n elements of an array of integers
- * @a: the array to print
- * @n: the number of elements of the array
+ * _strcpy - copies the string pointed to by src to dest
+ * @dest: buffer
+ * @src: pointer
  *
  * Return: always 0
  */
 
-void print_array(int *a, int n)
+char *_strcpy(char *dest, char *src);
 {
 	int x;
 
-	for (x = 0; x < n; x++)
+	x = 0;
+	if (src[x] != \0)
 	{
-		printf("%d", a[x]);
-
-		if (x < n - 1)
-		{
-			printf(", ");
-		}
+		*src = *dest;
 	}
-	printf("\n");
+	_putchar('\0')
+	return(dest);
 }
