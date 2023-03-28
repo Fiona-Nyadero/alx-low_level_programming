f0109ecae14389361235f146df26e7df97aa6f54
Fiona-Nyadero <fionanyadero@gmail.com>
2023-03-28 22:40:00 +0000

    8. Elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,22 +1,25 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
- * _strcpy - copies the string pointed to by src to dest
- * @dest: buffer
- * @src: pointer
+ * print_array - prints every other character of a string
+ * @a: an array of integers
+ * @n: number of elements to print
  *
  * Return: always 0
  */
 
-char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n)
 {
 	int x;
 
-	x = 0;
-	if (src[x] != \0)
+	for (x = 0; x < n; x++)
 	{
-		*src = *dest;
+		printf("%d", a[x]);
+		if (x < n - 1)
+		{
+			printf(", ");
+		}
 	}
-	_putchar('\0')
-	return(dest);
+	printf("\n");
 }
