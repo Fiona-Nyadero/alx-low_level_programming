cd485123b6fc7fdb15589058d0d11cb398aeba6e
Fiona-Nyadero <fionanyadero@gmail.com>
2023-04-15 00:18:21 +0000

    0. Fx allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * malloc_checked - allocates memory using malloc
+ * @b: variable
+ *
+ * Return: pointer to mem loc
+ * or 98 if fail
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *x;
+
+	x = malloc(b);
+
+	if (x == NULL)
+		exit(98);
+
+	return (x);
+}
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,6 @@
+0 TASK: 0. Fx allocates memory using malloc
+1 TASK: 1. Fx concatenates two strings
+2 TASK: 2. Fx allocates memory for an array
+3 TASK: 3. Fx creates an int array 
+4 TASK: 4. Fx reallocates a memory block
+5 TASK: 5. Program multiplies two +ve numbers
