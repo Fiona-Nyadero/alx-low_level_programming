285886070f1a60051ccebc68466a18cdde53ffb4
Fiona-Nyadero <fionanyadero@gmail.com>
2023-04-15 23:28:27 +0000

    3. Prgm multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Multiplies two numbers
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 1 if error
+ * else always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 0, y = 0;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+	
+		printf("%d\n", x * y);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
