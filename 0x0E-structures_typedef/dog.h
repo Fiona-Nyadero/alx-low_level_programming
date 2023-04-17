9c8d642759d8900478b3a2d4d32e45487ebc1d8f
Fiona-Nyadero <fionanyadero@gmail.com>
2023-04-17 23:46:01 +0000

    0. Define a structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - dog attributes
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
