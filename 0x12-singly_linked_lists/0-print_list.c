1101d7acd85c4d3c94b5816e93e54668f2ee8776
Fiona-Nyadero <fionanyadero@gmail.com>
2023-05-03 00:09:49 +0000

    0. Fx that prints all list elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,10 +16,10 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 
 		else
-			printf("%s\n", h->str);
+			printf("[%u] %s\n", h->len, h->str);
 
 		h = h->next;
 
