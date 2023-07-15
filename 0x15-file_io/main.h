125e7497e2c142c71f0f61e81a1a6dcfb3e70eec
Fiona-Nyadero <fionanyadero@gmail.com>
2023-07-15 00:39:06 +0000

    Task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,11 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -12,5 +15,16 @@ int append_text_to_file(const char *filename, char *text_content);
 int main(int ac, char *av[]);
 void close_file(int fildes);
 char *def_cont(char *file);
+/*void print_error_exit(char *msg);*/
+/*void print_elf_header_info(Elf32_Ehdr *header);*/
+void check_format(unsigned char *e_ident);
+void get_magic_num(unsigned char *e_ident);
+void get_class(unsigned char *e_ident);
+void get_data(unsigned char *e_ident);
+void get_version(unsigned char *e_ident);
+void get_abi(unsigned char *e_ident);
+void get_osabi(unsigned char *e_ident);
+void get_type(unsigned int e_type, unsigned char *e_ident);
+void get_entry_addr(unsigned long int e_entry, unsigned char *e_ident);
 
 #endif
