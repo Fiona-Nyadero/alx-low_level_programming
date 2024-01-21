e4226ba9e1a507349b0d791c3ab8e42bf431aeb3
Fiona-Nyadero <fionanyadero@gmail.com>
2024-01-21 02:22:30 +0000

    Fix 102.0

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,26 +16,13 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t hg = size - 1;
 	size_t probe_pos;
 
-	if (array == NULL || size == 0 || value < array[0] || value > array[size - 1])
-	{
-		printf("Value is out of range\n");
+	if (array == NULL || size == 0)
 		return (-1);
-	}
-
-	printf("Initial; lw: %lu, hg: %lu\n", lw, hg);
 
 	while (lw <= hg && value >= array[lw] && value <= array[hg])
 	{
 		probe_pos = lw + (((double)(hg - lw) / (array[hg] - array[lw]))
 				* (value - array[lw]));
-
-		printf("lw: %lu, hg: %lu, probe_pos: %lu", lw, hg, probe_pos);
-
-		if (probe_pos > hg)
-		{
-			printf("Value checked array[%lu] is out of range\n", probe_pos);
-			return (-1);
-		}
 
 		printf("Value checked array[%lu] = [%d]\n", probe_pos, array[probe_pos]);
 
@@ -48,16 +35,12 @@ int interpolation_search(int *array, size_t size, int value)
 		{
 			hg = probe_pos - 1;
 		}
-
 		else
 		{
 			lw = probe_pos + 1;
-
 		}
 	}
 
-	printf("Exited loop. lw: %lu, hg: %lu, probe_pos: %lu\n", lw, hg, probe_pos);
-
+	printf("Value checked array[%lu] is out of range\n", hg);
 	return (-1);
-
 }
